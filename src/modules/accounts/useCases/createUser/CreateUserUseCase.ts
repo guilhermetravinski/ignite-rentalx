c496c7f55db92f6640bbe0c406bf82ebde3b3d87
guilhermeTravinski <guitrafer@gmail.com>
2022-05-20 22:38:23 -0300

    Handle exceptions

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { hash } from 'bcrypt';
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 import { IUsersRepository } from '../../repositories/IUserRepository';
 
@@ -20,7 +21,7 @@ class CreateUserUseCase {
     const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error('User alreary exists');
+      throw new AppError('User alreary exists');
     }
 
     const passwordHash = await hash(password, 8);
